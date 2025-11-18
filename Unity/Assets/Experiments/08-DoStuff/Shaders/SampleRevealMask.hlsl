4cf249db76d3939b8454750a025182567fd898dd
Rohit Garg <12042989+64bits@users.noreply.github.com>
2025-11-17 19:12:14 -0800

    Permanently fixing weird issue due to cutout

--- Unity/Assets/Experiments/08-DoStuff/Shaders/SampleRevealMask.hlsl
+++ Unity/Assets/Experiments/08-DoStuff/Shaders/SampleRevealMask.hlsl
@@ -1,8 +1,15 @@
+#include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+
 TEXTURE2D(_CombinedMaskTex);
 SAMPLER(sampler_CombinedMaskTex);
 
 void SampleRevealMask_float(float4 ScreenPos, out float Mask)
 {
+    #if ( SHADERPASS == SHADERPASS_SHADOWCASTER )
+        Mask = 0.0;
+        return;
+    #endif
+
     float2 screenUV = ScreenPos.xy;
     screenUV = saturate(screenUV); // Clamp to 0-1 range
     Mask = SAMPLE_TEXTURE2D(_CombinedMaskTex, sampler_CombinedMaskTex, screenUV).g;
